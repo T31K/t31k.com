21162211764aa6867572d00b365f492b2afca6a9
T31K <t31kmunwong@gmail.com>
2024-05-22 12:12:31 +0800

    go

--- components/Spotify.jsx
+++ components/Spotify.jsx
@@ -9,6 +9,7 @@ function Spotify() {
   const [currentAlbum, setCurrentAlbum] = useState(
     'https://upload.wikimedia.org/wikipedia/en/2/27/Daft_Punk_-_Discovery.png'
   );
+  const [likedSongs, setLikedSongs] = useState([]);
 
   useEffect(() => {
     async function getCurrentSong() {
@@ -16,12 +17,17 @@ function Spotify() {
         const { data } = await axios.post(process.env.NEXT_PUBLIC_SPOTIFY_API_URL, {
           hash: process.env.NEXT_PUBLIC_SPOTIFY_HASH,
         });
+        console.log(data);
 
-        if (data !== '') {
-          setCurrentName(data?.item?.name);
-          setCurrentArtist(data?.item?.album?.artists[0]?.name);
-          setCurrentAlbum(data?.item?.album?.images[0]?.url);
-          console.log(data?.item?.album?.images[0]?.url);
+        if (data.current !== '') {
+          setCurrentName(data?.current?.item?.name);
+          setCurrentArtist(data?.current?.item?.album?.artists[0]?.name);
+          setCurrentAlbum(data?.current?.item?.album?.images[0]?.url);
+          console.log(data?.current?.item?.album?.images[0]?.url);
+        }
+
+        if (data.likedSongs !== '') {
+          setLikedSongs(data?.likedSongs?.items);
         }
       } catch (error) {
         console.error('Error fetching current song:', error);
@@ -33,10 +39,11 @@ function Spotify() {
 
   return (
     <div>
-      <div className="flex justify-center items-center flex-col">
+      <div className="flex justify-center items-center flex-col gap-2">
         <Image
           src="/spotify.png"
           alt="Spotify Logo"
+          className="mb-3"
           width={45}
           height={45}
         />
@@ -50,8 +57,8 @@ function Spotify() {
           className="mb-6"
           height={200}
         />
-        <p className="text-center whitespace-nowrap !mt-6 text-2xl">{currentName?.slice(0, 16)}</p>
-        <p className="text-center whitespace-nowrap text-lg mt-[-15px]">{currentArtist}</p>
+        <p className="text-center whitespace-nowrap !mt-6 text-xl">{currentName?.slice(0, 16)}</p>
+        <p className="text-center whitespace-nowrap text-md mt-[-15px]">{currentArtist}</p>
         <div className="flex justify-center items-center gap-4 ">
           <Image
             src="/prev.webp"
@@ -74,6 +81,31 @@ function Spotify() {
             height={35}
           />
         </div>
+      </div>
+      <div
+        className="lists"
+        style={{ padding: '20px' }}
+      >
+        <div className="flex items-center justify-center mb-5">
+          <i className="nes-icon is-medium heart"></i>
+          <p style={{ textAlign: 'center', fontSize: '20px', margin: 0 }}>Top tracks this week</p>
+        </div>
+        <ul
+          className="nes-list is-circle"
+          style={{ marginTop: '15px' }}
+        >
+          {likedSongs?.map((likedSong, key) => (
+            <>
+              <li
+                key={key}
+                className="whitespace-nowrap"
+                style={{ margin: '5px 0' }}
+              >
+                {likedSong.track.artists[0].name} - {likedSong.track.name?.split('(')[0].trim()}
+              </li>
+            </>
+          ))}
+        </ul>
       </div>
     </div>
   );
