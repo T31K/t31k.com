fd354555a9d444a05844ef536a61d784d3b5ff44
T31K <t31kmunwong@gmail.com>
2024-11-20 10:43:38 +0800

    add links

---
+++ app/links/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function page() {
+  return (
+    <main className=" flex min-h-screen flex-col items-center gap-y-6 py-24 px-16 bg-black text-white relative">
+      <h1 className="text-2xl">Links</h1>
+      <a href="https://t31k.com/">T31K</a>
+      <a href="https://ourbabyai.com">Our Baby AI</a>
+      <a href="https://getharmonize.app">Harmonize</a>
+      <a href="https://calorieasy.app">Calorieasy</a>
+      <a href="https://aiheadshotgenerator.com">AI Headshot Generator</a>
+      <a href="https://listskit.com">Lists Kit</a>
+      <a href="https://hackalumpur.com">Hacka Lumpur</a>
+      <a href="https://clemanteemusic.com">Clemantee Music</a>
+      <a href="https://fixitjerry.com">Fix It Jerry</a>
+      <a href="https://mirabelledecoupage.com">Mirabelle Decoupage</a>
+      <a href="https://dokeyai.com/">DokeyAI</a>
+    </main>
+  );
+}
+
+export default page;
