6982622aa6c148c838c37a4275a581ce21cce985
T31K <t31kmunwong@gmail.com>
2025-03-14 17:08:39 +0800

    try

--- app/meownubar/page.tsx
+++ app/meownubar/page.tsx
@@ -50,6 +50,7 @@ function Page() {
           onChange={handleChange}
           required
         />
+
         <button
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded-full mx-auto block !font-mono mt-4"
