68d37d5b8b88080bbb0b63f770c8d5c1890f1d1a
T31K <t31kmunwong@gmail.com>
2025-09-10 14:38:31 +0800

    update vinyl collection

---
+++ components/TurnTable.tsx
@@ -0,0 +1,100 @@
+import { useRef } from 'react';
+
+const TurnTable = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (audioRef.current) {
+      audioRef.current.volume = parseFloat(event.target.value);
+    }
+  };
+
+  const playMusic = () => {
+    if (audioRef.current) {
+      if (audioRef.current.paused) {
+        void audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  };
+
+  return (
+    <>
+      <p className="text-center whitespace-nowrap mt-4 text-2xl ml-2 text-dark dark:text-dark">Vinyl Collection</p>
+      <div className="grid grid-cols-3 gap-x-2">
+        {/* <div className="vinyl-jacket">
+          <div className="vinyl-wrapper">
+            <div className="vinyl-record"></div>
+          </div>
+        </div> */}
+        <img
+          src="/elvin.jpeg"
+          alt="elvin"
+          className="w-full h-full object-cover hover:scale-105 transition-all duration-300 hover:brightness-125"
+        />
+        <img
+          src="/blue_swede.jpeg"
+          alt="elvin"
+          onClick={() => {
+            const needle = document.getElementById('headshell');
+            if (needle) needle.click();
+          }}
+          className="w-full h-full object-cover hover:scale-105 transition-all duration-300 hover:brightness-125"
+        />
+        <img
+          src="/metallica.webp"
+          alt="elvin"
+          className="w-full h-full object-cover hover:scale-105 transition-all duration-300 hover:brightness-125"
+        />
+      </div>
+
+      <div className="record-player relative">
+        <section
+          className="message-left"
+          style={{ zIndex: 50, position: 'absolute', right: -50, top: -20 }}
+        >
+          {/* <div className="nes-balloon from-left !py-1 !px-2">
+            <p className="!text-[10px] text-dark dark:text-dark">Drop the needle</p>
+          </div> */}
+        </section>
+
+        <input
+          type="checkbox"
+          id="headshell"
+          onClick={playMusic}
+        />
+        <label
+          className="headshell"
+          htmlFor="headshell"
+        ></label>
+
+        <audio
+          id="player"
+          ref={audioRef}
+        >
+          <source
+            src="/hooked.mp3"
+            type="audio/mpeg"
+          />
+        </audio>
+
+        <input
+          type="range"
+          max="1"
+          min="0"
+          step="0.1"
+          id="volume-control"
+          onChange={handleVolumeChange}
+        />
+
+        <div className="plinth"></div>
+        <div className="platter"></div>
+        <div className="vinyl"></div>
+        <div className="top-circle"></div>
+      </div>
+    </>
+  );
+};
+
+export default TurnTable;
